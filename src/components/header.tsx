6300bc269dca303bf9b0b0070075ed9bea4c657a
Lee Wan Geun <wglee21g@gmail.com>
2020-03-10 00:24:50 +0000

    RSS 데이터 추가

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -9,32 +9,43 @@ const navItems: { label: string; page?: string }[] = [
   { label: 'StudyNote', page: '/studynote' },
 ]
 
-const ogImageUrl = 'https://notion-blog.now.sh/og-image.png'
+const ogImageUrl = 'https://icednut-github-io-notion.now.sh/og-image.jpg'
 
 export default ({ titlePre = '', category = '' }) => {
-  // const { pathname } = useRouter()
-
-  // if (pathname === '/') {
-  //   category = 'DevLog'
-  // } else if (pathname === '/studynote') {
-  //   category = 'StudyNote'
-  // }
-
   return (
     <header>
       <Head>
-        <title>{titlePre ? `${titlePre} |` : ''} My Notion Blog</title>
+        <title>
+          {titlePre ? `${titlePre} |` : ''} Icednut's Space Notion Blog
+        </title>
         <meta
           name="description"
-          content="An example Next.js site using Notion for the blog"
+          content="Web Backend Development, Study Note, Life Log using Notion"
         />
-        <meta name="og:title" content="My Notion Blog" />
+        <meta name="og:title" content="Icednut's Space Notion Blog" />
         <meta property="og:image" content={ogImageUrl} />
-        <meta name="twitter:site" content="@_ijjk" />
+        <meta name="twitter:site" content="@crazybnn" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:image" content={ogImageUrl} />
       </Head>
       <div className="relative pt-12 mb-20 bg-white">
+        <div className="absolute" style={{ left: '28px', top: '28px' }}>
+          <Link href="/atom">
+            <a target="_blank">
+              <svg
+                id="Bold"
+                height="14"
+                viewBox="0 0 24 24"
+                width="14"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <circle cx="3.429" cy="20.571" r="3.429" />
+                <path d="m11.429 24h4.57c0-8.821-7.178-15.999-15.999-16v4.572c6.302.001 11.429 5.126 11.429 11.428z" />
+                <path d="m24 24c0-13.234-10.766-24-24-24v4.571c10.714 0 19.43 8.714 19.43 19.429z" />
+              </svg>
+            </a>
+          </Link>
+        </div>
         <img
           src="/wglee2.png"
           alt="wglee"
