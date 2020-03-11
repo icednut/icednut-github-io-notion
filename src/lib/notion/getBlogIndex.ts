6a1c66debce91ef05d8731583e3fbfc82926d9be
Lee Wan Geun <wglee21g@gmail.com>
2020-03-11 13:09:53 +0000

    코드 복사하기 기능 추가

--- src/lib/notion/getBlogIndex.ts
+++ src/lib/notion/getBlogIndex.ts
@@ -57,30 +57,30 @@ export default async function getBlogIndex(
     }
 
     // only get 10 most recent post's previews
-    const postsKeys = Object.keys(postsTable).splice(0, 10)
+    // const postsKeys = Object.keys(postsTable).splice(0, 10)
 
-    const sema = new Sema(3, { capacity: postsKeys.length })
+    // const sema = new Sema(3, { capacity: postsKeys.length })
 
-    if (previews) {
-      await Promise.all(
-        postsKeys
-          .sort((a, b) => {
-            const postA = postsTable[a]
-            const postB = postsTable[b]
-            const timeA = postA.Date
-            const timeB = postB.Date
-            return Math.sign(timeB - timeA)
-          })
-          .map(async postKey => {
-            await sema.acquire()
-            const post = postsTable[postKey]
-            post.preview = post.id
-              ? await getPostPreview(postsTable[postKey].id)
-              : []
-            sema.release()
-          })
-      )
-    }
+    // if (previews) {
+    //   await Promise.all(
+    //     postsKeys
+    //       .sort((a, b) => {
+    //         const postA = postsTable[a]
+    //         const postB = postsTable[b]
+    //         const timeA = postA.Date
+    //         const timeB = postB.Date
+    //         return Math.sign(timeB - timeA)
+    //       })
+    //       .map(async postKey => {
+    //         await sema.acquire()
+    //         const post = postsTable[postKey]
+    //         post.preview = post.id
+    //           ? await getPostPreview(postsTable[postKey].id)
+    //           : []
+    //         sema.release()
+    //       })
+    //   )
+    // }
 
     if (useCache) {
       writeFile(cacheFile, JSON.stringify(postsTable), 'utf8').catch(() => {})
