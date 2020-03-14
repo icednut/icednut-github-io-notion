a0451663e0ea59c0b09602884d83d8b99f94bd6a
Lee Wan Geun <wglee21g@gmail.com>
2020-03-14 01:41:52 +0000

    GA연동 오류 수정

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -33,7 +33,7 @@ export default ({ titlePre = '', category = '' }) => {
           src="https://www.googletagmanager.com/gtag/js?id=UA-91631073-2"
         ></script>
         <script>
-          window.dataLayer = window.dataLayer || []; function gtag()
+          window.dataLayer = window.dataLayer || [] function gtag()
           {dataLayer.push(arguments)}
           gtag('js', new Date()) gtag('config', 'UA-91631073-2')
         </script>
