103ef430f2bdd50595b58b0659f819922c5b9613
Lee Wan Geun <wglee21g@gmail.com>
2020-03-21 06:34:01 +0000

    코드에 공간 추가

--- src/components/code.tsx
+++ src/components/code.tsx
@@ -16,7 +16,7 @@ const Code = ({ children, language = 'javascript' }) => {
         </Clipboard>
         <pre>
           <code
-            className="overflow-auto block my-1 p-5 text-sm"
+            className="overflow-auto block my-1 px-5 py-10 text-sm"
             style={{ backgroundColor: '#2d2d2d', color: '#ccc' }}
             dangerouslySetInnerHTML={{
               __html: Prism.highlight(
